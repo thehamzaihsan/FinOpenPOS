6f30debd7d237151bddf80c8c1e1051d996ce94b
thehamzaihsan <thehamzaihsan@gmail.com>
2025-01-30 17:34:12 +0500

    homepage redirects to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/admin");
+  }, [router]);
+
+  return null;
 }
