2cf8b574a1cd7e3e920954806b01ad5d072f25b6
thehamzaihsan <thehamzaihsan@gmail.com>
2025-04-09 22:20:39 +0500

    build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Analyt from "@/components/Analytics";
+
 const inter = Inter({ subsets: ["latin"] });
 //Import Mixpanel SDK
 
--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { initMixpanel } from "../lib/mixpanelClient";
+import { initMixpanel } from "../lib/mixpanelClient.js";
 import { useEffect } from "react";
 export default function Analyt() {
   useEffect(() => {
